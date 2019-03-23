904b8c8941c95c6c9ce703d857312e632443f1ef
Anantha prakash <ananthaprakashb@gmail.com>
2019-03-23 12:14:33 +0530

    Updates

---
+++ precache-manifest.87b849518f5ac3a88ff904283edce7a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myweb/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7030987f34f11e07ef84",
+    "url": "/myweb/static/js/runtime~main.ccbb7580.js"
+  },
+  {
+    "revision": "0f30cacdd66edb1a25b1",
+    "url": "/myweb/static/js/main.609da5fa.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/myweb/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "0f30cacdd66edb1a25b1",
+    "url": "/myweb/static/css/main.29f710d7.chunk.css"
+  },
+  {
+    "revision": "55ebb77d9c726d6706a63013746ae60a",
+    "url": "/myweb/index.html"
+  }
+];
